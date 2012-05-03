422bf287c990add4945f0a0114c379b4a6b68573
Tyler Much <tylermuch@gmail.com>
2012-05-02 23:35:53 -0500

    new_commit

--- address_book.rb
+++ address_book.rb
@@ -1,26 +1,43 @@
 require_relative 'address_entry'
 
-book = Array.new
+$book = Array.new
+cs = {
+  'list' => -> {List()},
+	'write'=> -> {Write()},
+  'exit' => -> {Exit()}
+}
 
 #This reads file
 File.open('book.txt') do |f|
   count = f.gets.chop.to_i
   count.times do
-    book << AddressEntry.new(f.gets.delete("\n"), f.gets.delete("\n"))
+    $book << AddressEntry.new(f.gets.delete("\n"), f.gets.delete("\n"))
   end
 end
 
-=begin
-#Check file size
-File.open('book.txt') do |r|
-  $count = r.readlines.size/2
-  r.close
-end
-=end
-
-#This prints entries
-def List(book)
-  book.each_index do |entry|
-    puts "#{entry}: #{book[entry].name} -- #{book[entry].address}"
+def List
+  $book.each_index do |entry|
+    puts "#{entry}: #{$book[entry].name} -- #{$book[entry].address}"
   end
+end
+
+def Exit
+  Process.exit
+end
+
+def Write
+		File.open('out.txt', 'w') do |f|
+			$book.each do |entry|
+				f.puts entry.name
+				f.puts entry.address
+			end
+		end
+		puts "Saved Address Book Successfully!"
+end
+
+#MAIN
+while true
+  print "Command?: "
+  com = gets.chop.downcase
+  cs[com].call
 end
